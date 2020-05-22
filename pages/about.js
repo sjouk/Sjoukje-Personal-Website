d8ccab109151d67fc4bb54b4fd8ffd6d6cae0c48
sijlstra <sjoukjeijlstra@gmail.com>
2020-05-22 20:51:18 +0200

    .

---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import Layout from '../components/Layout'
+
+const About = ({ title, description, ...props }) => {
+    return (
+        <>
+            <Layout pageTitle={`${title} | About`} description={description}>
+                <h1 className="title">Welcome to my blog!</h1>
+
+                <p className="description">{description}</p>
+
+                <p>This is my blog! A description</p>
+            </Layout>
+        </>
+    )
+}
+
+export default About
+
+export async function getStaticProps() {
+    const configData = await import(`../siteconfig.json`)
+
+    return {
+        props: {
+            title: configData.default.title,
+            description: configData.default.description
+        }
+    }
+}
